4d4f1c3f20713323bd9c6b8eac76c8d1c15e0fd2
Richard You <richardyou94@gmail.com>
2023-02-17 11:08:41 -0800

    passport and session implemented

--- app.js
+++ app.js
@@ -5,13 +5,26 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.use(
+  session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session);
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
@@ -34,13 +47,9 @@ app.get("/register", function (req, res) {
   res.render("register");
 });
 
-app.post("/register", function (req, res) {
+app.post("/register", function (req, res) {});
 
-});
-
-app.post("/login", function (req, res) {
- 
-});
+app.post("/login", function (req, res) {});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
