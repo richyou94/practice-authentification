b07ae9edcad5c9a41d4e6e5e28eeef6f412a5b4f
Richard You <richardyou94@gmail.com>
2023-02-16 21:46:39 -0800

    done with environment variable

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
