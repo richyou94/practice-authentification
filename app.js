a7427f725db5eecbff4a70b2eb67d00e821b1d81
Richard You <richardyou94@gmail.com>
2023-02-17 11:04:35 -0800

    empty the both poast for login and register for using passport middleware

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
 
 const app = express();
 
@@ -37,43 +35,11 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash,
-    });
 
-    newUser.save(function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
-  });
 });
 
 app.post("/login", function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  User.findOne({ email: username }, function (err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        bcrypt.compare(
-          password,
-          foundUser.password,
-          function (error, isPasswordCorrect) {
-            if (isPasswordCorrect === true) {
-              res.render("secrets");
-            }
-          }
-        );
-      }
-    }
-  });
+ 
 });
 
 app.listen(3000, function () {
